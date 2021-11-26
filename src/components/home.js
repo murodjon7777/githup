21b5defe0cd72001fdbf621efb4c48cb3b08b29b
murodjon7777 <murodjonnomonov2000@gmail.com>
2021-11-27 01:24:59 +0500

    boys

---
+++ src/components/home.js
@@ -0,0 +1,10 @@
+import Navbar from "./select/Navbar"
+
+const Home=()=>{
+    return(
+        <>
+        <Navbar/>
+        </>
+    )
+}
+export default Home
---
+++ src/components/select/Navbar.js
@@ -0,0 +1,27 @@
+const Navbar=()=>{
+    return(
+        <>
+        <div className="navbar">
+            <div className="container">
+                <div className="nav-content">
+                    <div className="logo">
+                        <img src="/img/logo.png"/>
+                    
+                    </div>
+                    <div className="nav-ul">
+                        <ul>
+                            <li>Home</li>
+                            <li>Download</li>
+                            <li>Featurtes</li>
+                            <li>Pricing</li>
+                            <li>Testimonials</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    
+    )
+}
+export default Navbar
